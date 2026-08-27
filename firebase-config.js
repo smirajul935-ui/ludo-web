bed7aec5b6e673c7658b6a2a65b9c11fbf81cf0e
smirajul935-ui <smirajul935@gmail.com>
2026-08-27 17:37:39 +0530

    Add Firebase configuration and initialization

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAPug7NEBR9oOuKnmF2m7m7TUiZI9TZVQo",
+  authDomain: "ludo-e5cfc.firebaseapp.com",
+  databaseURL: "https://ludo-e5cfc-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ludo-e5cfc",
+  storageBucket: "ludo-e5cfc.firebasestorage.app",
+  messagingSenderId: "596873684153",
+  appId: "1:596873684153:web:89d485c778855904906768"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+const auth = firebase.auth();
